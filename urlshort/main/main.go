f835f8623f0e256ffa5187d65710e3716759d631
sturmcmaper <sturmcmaper@gmail.com>
2024-09-25 14:31:19 +0200

    url-Shorter

---
+++ urlshort/main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"urlshort"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/test": "https://google.de",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /sturm
+  url: https://sturmcamper.github.io/
+- path: /github
+  url: https://github.io/
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
